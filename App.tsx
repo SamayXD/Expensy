169f0c2aadfa2c940520c887fe2c448679a81a98
Samay Navale <samaynavale03@gmail.com>
2024-07-21 14:38:05 +0530

    chore: Add SQLite database setup and navigation to Home screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Suspense, useEffect, useState } from 'react';
+import { ActivityIndicator, Text, View } from 'react-native';
+import * as FileSystem from 'expo-file-system'
+import { Asset } from 'expo-asset'
+import { SQLiteProvider } from 'expo-sqlite';
+import Home from './screens/Home';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator()
+
+const loadDatabase = async () =>{
+  const dbName = "mySQLiteDB.db";
+  const dbAsset = require("./assets/mySQLiteDB.db")
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath)
+  if(!fileInfo.exists){
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,{
+        intermediates : true
+      }
+    );
+    await FileSystem.downloadAsync(dbUri, dbFilePath);
+  }
+}
 
 export default function App() {
+  const [dbLoaded, setDbLoaded] = useState<boolean>(false)
+
+  useEffect(()=>{
+      loadDatabase().then(()=>{
+        setDbLoaded(true)
+      }).catch((e)=>console.log("ERROR: ",e))
+  },[])
+
+  if(!dbLoaded){
+    return (
+      <View style={{flex: 1, justifyContent: "center", alignItems : "center"}}>
+        <ActivityIndicator size={"large"} />
+        <Text>
+        Loading
+        </Text>
+      </View>
+    )
+  }
   return (
-    <View style={styles.container}>
-      <Text>Hello Navale!</Text>
-      <StatusBar style="auto" />
+   <NavigationContainer>
+    <React.Suspense
+    fallback= {
+      <View style={{flex: 1, justifyContent: "center", alignItems : "center"}}>
+      <ActivityIndicator size={"large"} />
+      <Text>
+      Loading
+      </Text>
     </View>
+    }
+    >
+    <SQLiteProvider 
+    databaseName="mySQLiteDB.db"
+      useSuspense
+    > 
+    <Stack.Navigator>
+      <Stack.Screen name = "Home" component={Home} 
+        options={{
+          headerTitle : "Expensy",
+          headerLargeTitle: true,
+        }}
+      />
+    </Stack.Navigator>
+      
+    </SQLiteProvider>
+   </React.Suspense>
+   </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
